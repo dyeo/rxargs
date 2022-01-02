efc409e60048ef975657a905b14f5ab84db99d70
Dan <dan@dyeo.net>
2022-01-01 23:07:01 -0500

    Add man page; setup.py

--- rxargs/__main__.py
+++ rxargs/__main__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 """
 Interpret standard input using PATTERN, then execute COMMAND as a substitution of that input.
-Note that this program uses python regular expressions. See <https://docs.python.org/3/library/re.html#regular-expression-syntax> for more info.
 """
 
 import argparse
@@ -11,19 +10,37 @@ from sys import argv, stdin
 
 __version__="0.1.0"
 
-argp = argparse.ArgumentParser(description=__doc__)
-argp.add_argument('pattern', metavar='PATTERN', type=re.compile, help='the pattern to parse from stdin')
-argp.add_argument('command', metavar='COMMAND', type=str, help='the command to execute using the regex substitution')
-argp.add_argument('-n, --display', dest='display', action='store_true', help='display command result without running it')
-argp.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__)
-args = argp.parse_args()
+__epilog__="""
+This program uses python regular expressions.
+See <https://docs.python.org/3/library/re.html#regular-expression-syntax> for more info.
+"""
 
-for line in stdin:
-    line = line.rstrip('\r\n')
-    cmd = args.pattern.sub(args.command, line).rstrip('\r\n')
-    if args.display:
-        print(f"'{line.strip()}' -> '{cmd}'")
-    else:
-        stm = popen(cmd)
-        for l in stm.readlines():
-            print(l, end='')
+
+def parse_args():
+    argp = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        prog="rxargs",
+        description=__doc__, 
+        epilog=__epilog__
+    )
+    argp.add_argument('pattern', metavar='PATTERN', type=re.compile, help='the pattern to parse from stdin')
+    argp.add_argument('command', metavar='COMMAND', type=str, help='the command to execute using the regex substitution')
+    argp.add_argument('-n, --display', dest='display', action='store_true', help='display command result without running it')
+    argp.add_argument('-v', '--version', action='version', version='%(prog)s ' + __version__)
+    args = argp.parse_args()
+    return args
+    
+def run():
+    args = parse_args()
+    for line in stdin:
+        line = line.rstrip('\r\n')
+        cmd = args.pattern.sub(args.command, line).rstrip('\r\n')
+        if args.display:
+            print(f"'{line.strip()}' -> '{cmd}'")
+        else:
+            stm = popen(cmd)
+            for l in stm.readlines():
+                print(l, end='')
+
+if __name__ == "__main__":
+    run()    
